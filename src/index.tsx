80860da9eb5618d073bf7da75d3231f1dffcce9c
sino330 <narutokozo330@gmail.com>
2021-12-05 09:23:12 +0900

    2021/12/05

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Globalstyle } from './GlobalStyle'
+import { App } from './App'
+
+ReactDOM.render(
+  <>
+    <Globalstyle />
+    <App />
+  </>,
+  document.getElementById('app'),
+)
