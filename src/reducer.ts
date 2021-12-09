c3207a093a397c029c323c3f504355b09b969414
sino330 <narutokozo330@gmail.com>
2021-12-09 11:46:27 +0900

    2021/12/09

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,12 +1,25 @@
 import { Reducer } from 'redux'
 import produce from 'immer'
+import { sortBy } from './util'
+import { CardID, ColumnID } from './api'
 
 export type State = {
   filterValue: string
+  columns?: {
+    id: ColumnID
+    title?: string
+    text?: string
+    cards?: {
+      id: CardID
+      text?: string
+    }[]
+  }[]
+  cardsOrder: Record<string, CardID | ColumnID | null>
 }
 
 const initialState: State = {
   filterValue: '',
+  cardsOrder: {},
 }
 
 export type Action = {
@@ -15,6 +28,26 @@ export type Action = {
     value: string
   }
 }
+| {
+      type: 'App.SetColumns'
+      payload: {
+        columns: {
+          id: ColumnID
+          title?: string
+          text?: string
+        }[]
+      }
+    }
+  | {
+      type: 'App.SetCards'
+      payload: {
+        cards: {
+          id: CardID
+          text?: string
+        }[]
+        cardsOrder: Record<string, CardID | ColumnID | null>
+      }
+    }
 
 export const reducer: Reducer<State, Action> = produce(
   (draft: State, action: Action) => {
@@ -25,6 +58,27 @@ export const reducer: Reducer<State, Action> = produce(
               draft.filterValue = value
               return
           }
+
+          case 'App.SetColumns': {
+            const { columns } = action.payload
+
+            draft.columns = columns
+        return
+      }
+
+      case 'App.SetCards': {
+        const { cards: unorderedCards, cardsOrder } = action.payload
+
+        draft.cardsOrder = cardsOrder
+        draft.columns?.forEach(column => {
+          column.cards = sortBy(unorderedCards, cardsOrder, column.id)
+        })
+        return
+      }
+
+      default: {
+        const _: never = action
+      }
       }
   },
   initialState,
---
+++ test/reducer.test.ts
@@ -0,0 +1,104 @@
+import baretest from 'baretest'
+import assert from 'assert'
+import produce from 'immer'
+import { reducer, State } from '../src/reducer'
+import { ColumnID, CardID } from '../src/api'
+
+const test = baretest('reducer')
+// setImmediate(() => test.run())
+
+const initialState: State = {
+  filterValue: '',
+  cardsOrder: {},
+}
+
+test('Filter.SetFilter', async () => {
+  const prev = produce(initialState, draft => {
+    draft.filterValue = 'hello'
+  })
+
+  const next = reducer(prev, {
+    type: 'Filter.SetFilter',
+    payload: {
+      value: 'welcome',
+    },
+  })
+
+  const expected = produce(prev, draft => {
+    draft.filterValue = 'welcome'
+  })
+
+  assert.deepStrictEqual(next, expected)
+})
+
+test.run()
+
+test('App.SetCards', async () => {
+  const prev = produce(initialState, draft => {
+    draft.columns = [
+      {
+        id: 'A' as ColumnID,
+      },
+      {
+        id: 'B' as ColumnID,
+      },
+    ]
+  })
+
+  const next = reducer(prev, {
+    type: 'App.SetCards',
+    payload: {
+      cards: [
+        {
+          id: '3' as CardID,
+        },
+        {
+          id: '2' as CardID,
+        },
+        {
+          id: '1' as CardID,
+        },
+      ],
+      cardsOrder: {
+        A: '1' as CardID,
+        '1': '2' as CardID,
+        '2': 'A' as CardID,
+        B: '3' as CardID,
+        '3': 'B' as CardID,
+      },
+    },
+  })
+
+  const expected = produce(prev, draft => {
+    draft.cardsOrder = {
+      A: '1' as CardID,
+      '1': '2' as CardID,
+      '2': 'A' as CardID,
+      B: '3' as CardID,
+      '3': 'B' as CardID,
+    }
+    draft.columns = [
+      {
+        id: 'A' as ColumnID,
+        cards: [
+          {
+            id: '1' as CardID,
+          },
+          {
+            id: '2' as CardID,
+          },
+        ],
+      },
+      {
+        id: 'B' as ColumnID,
+        cards: [
+          {
+            id: '3' as CardID,
+          },
+        ],
+      },
+    ]
+  })
+
+  assert.deepStrictEqual(next, expected)
+})
